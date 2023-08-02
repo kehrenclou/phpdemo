521491de3f1198b4e1e4ad397043b94a747c5d53
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-08-02 12:26:59 -0700

    Lesson 5 Conditionals

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-  
+
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
+
 <body>
+    <?php
+    $name = "Dark Matter";
+    $read = true;
+    if ($read) {
+        $message = "You have read $name";
+    } else {
+        $message = "You need to read $name!";
+    }
+
+    ?>
     <h1>
-        <?php 
-        
-        ?>
+        <?= $message; ?>
     </h1>
 </body>
+
 </html>
